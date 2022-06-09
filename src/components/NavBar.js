902bca57ed13749ed0830dbf663e231c6a0cf094
Trevor Vardeman <trevortx@gmail.com>
2022-06-08 23:44:16 -0700

    fixed padding

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,7 +4,7 @@ import Button from 'react-bootstrap/Button'
 
 function NavBar({ logout }) {
   return (
-    <Stack direction="horizontal" gap={3} style={{ backgroundColor: "skyblue", justifyContent: "space-between", borderBottom: "2px solid black" }}>
+    <Stack direction="horizontal" gap={3} style={{ backgroundColor: "skyblue", justifyContent: "space-between", borderBottom: "2px solid black", paddingLeft: "10px", paddingRight: "10px" }}>
       <Link className="font-link" style={{ fontSize: "30px", textDecoration: "none", color: "black" }} to="/">Lyric Liker</Link>
       <NavLink className="btn btn-outline-dark btn-sm" to="/playlists">Playlists</NavLink>
       <NavLink className="btn btn-outline-dark btn-sm" to="/saved-lyrics">Saved Lyrics</NavLink>
