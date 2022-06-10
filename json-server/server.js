3d47209b9077d20f5f9b513f4751fafa7640fcde
Trevor Vardeman <trevortx@gmail.com>
2022-06-09 21:24:20 -0700

    readme correction

--- json-server/server.js
+++ json-server/server.js
@@ -10,12 +10,6 @@ server.use(jsonServer.bodyParser);
 server.use(middlewares);
 server.use(router);
 
-server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3000')
-  res.header('Access-Control-Allow-Headers', '*')
-  next()
-})
-
 const PORT = process.env.PORT || 3000;
 
 server.listen(PORT, () => {
